1ec2623241f8bafe7aaf2aa6d461bc3e912f2b7b
androidcodehunter <sharif.iit.du@gmail.com>
2015-02-23 19:30:25 +0600

    add volley library and relevant code in BanglaHadithApp.java

--- app/src/main/java/com/hadithbd/banglahadith/BanglaHadithApp.java
+++ app/src/main/java/com/hadithbd/banglahadith/BanglaHadithApp.java
@@ -2,7 +2,12 @@ package com.hadithbd.banglahadith;
 
 import android.app.Application;
 import android.graphics.Typeface;
+import android.text.TextUtils;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.Volley;
 import com.hadithbd.banglahadith.util.Constants;
 
 /**
@@ -10,7 +15,13 @@ import com.hadithbd.banglahadith.util.Constants;
  */
 public class BanglaHadithApp extends Application {
 
+    public static final String TAG = "VolleyTag";
+
     public static Typeface banglaHadithFont;
+
+    private RequestQueue mRequestQueue;
+
+    private static BanglaHadithApp mInstance;
 
     @Override
     public void onCreate() {
@@ -19,5 +30,39 @@ public class BanglaHadithApp extends Application {
         banglaHadithFont = Typeface.createFromAsset(getAssets(),
                 Constants.FONT_FOLDER_LOCATION + Constants.FONT_NAME);
 
+        mInstance = this;
+
+    }
+
+    public static synchronized BanglaHadithApp getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> request, String tag) {
+        request.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+
+        VolleyLog.d("Adding to request queue %s", request.getUrl());
+
+        getRequestQueue().add(request);
+    }
+
+
+    public <T> void addToRequestQueue(Request<T> request) {
+        request.setTag(TAG);
+        getRequestQueue().add(request);
+    }
+
+    public void cancelPendingRequest(Object tag){
+        if (mRequestQueue!=null){
+            mRequestQueue.cancelAll(tag);
+        }
     }
 }
