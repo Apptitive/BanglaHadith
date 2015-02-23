3d85f234cb52c10ed4962fd3b66a24bee79ab091
androidcodehunter <sharif.iit.du@gmail.com>
2015-02-23 14:01:22 +0600

    add hadit detail activity

--- app/src/main/java/com/hadithbd/banglahadith/adapters/HadithDetailListAdapter.java
+++ app/src/main/java/com/hadithbd/banglahadith/adapters/HadithDetailListAdapter.java
@@ -1,32 +1,55 @@
 package com.hadithbd.banglahadith.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.hadithbd.banglahadith.R;
+import com.hadithbd.banglahadith.views.BanglaTextView;
+
+import java.util.List;
 
 /**
  * Created by Sharif on 2/19/2015.
  */
-public class HadithDetailListAdapter extends RecyclerView.Adapter<HadithDetailListAdapter.ViewHolder>{
+public class HadithDetailListAdapter extends RecyclerView.Adapter<HadithDetailListAdapter.ViewHolder> {
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return null;
+    private List<String> mHadithDetailLists;
+
+    public HadithDetailListAdapter(List<String> hadithDetailLists) {
+        mHadithDetailLists = hadithDetailLists;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
+        final View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.item_hadith_detail_lists, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        viewHolder.mTextDrawable.setBanglaText(mHadithDetailLists.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mHadithDetailLists != null ? mHadithDetailLists.size() : 0;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public BanglaTextView mTextDrawable;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            mTextDrawable = (BanglaTextView) itemView.findViewById(R.id.text_drawable);
         }
     }
+
+
 }
--- app/src/main/java/com/hadithbd/banglahadith/util/Utils.java
+++ app/src/main/java/com/hadithbd/banglahadith/util/Utils.java
@@ -1,10 +1,19 @@
 package com.hadithbd.banglahadith.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Sharif on 2/22/2015.
  */
 public class Utils {
 
-
-
+    public static List<String>getDummyHaditsData(){
+        List<String>datas = new ArrayList<>();
+        datas.add("1");
+        datas.add("2");
+        datas.add("3");
+        datas.add("4");
+        return datas;
+    }
 }
