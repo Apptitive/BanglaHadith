0fa0b3b8da4fb50b9ba6f9345cb0f3bf2cab2ca5
Sharifur Rahman <sharif.iit.du@gmail.com>
2015-03-02 13:24:16 +0600

    add chapter in BookListActivity.java

--- app/src/main/java/com/hadithbd/banglahadith/ui/BookListActivity.java
+++ app/src/main/java/com/hadithbd/banglahadith/ui/BookListActivity.java
@@ -8,19 +8,27 @@ import android.support.v7.widget.Toolbar;
 
 import com.hadithbd.banglahadith.R;
 import com.hadithbd.banglahadith.adapters.BookListAdapter;
+import com.hadithbd.banglahadith.database.DbManager;
 import com.hadithbd.banglahadith.util.Constants;
+import com.hadithbd.banglahadith.viewmodel.HadithBookInfo;
 
-public class BookListActivity extends BaseActivity implements BookListAdapter.BookItemClickListener{
+import java.util.List;
+
+public class BookListActivity extends BaseActivity implements BookListAdapter.BookItemClickListener {
 
     private RecyclerView mRecyclerView;
 
     private BookListAdapter mBookListAdapter;
     private Toolbar mToolbar;
 
+    private List<HadithBookInfo> mBookInfoList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+
+        mBookInfoList = DbManager.getInstance().getAllHadithBookInfo();
 
         setHomeBackground();
         initViews();
@@ -37,7 +45,7 @@ public class BookListActivity extends BaseActivity implements BookListAdapter.Bo
 
     private void initRecyclerAdapter() {
         mRecyclerView.setHasFixedSize(true);
-        mBookListAdapter = new BookListAdapter(null);
+        mBookListAdapter = new BookListAdapter(this, mBookInfoList);
         mRecyclerView.setLayoutManager(new GridLayoutManager(this, Constants.NUMBER_OF_GRID_COLUMNS));
         mBookListAdapter.setBookItemClickListener(this);
         mRecyclerView.setAdapter(mBookListAdapter);
@@ -51,7 +59,9 @@ public class BookListActivity extends BaseActivity implements BookListAdapter.Bo
 
     @Override
     public void onBookItemClicked(int position) {
+        final HadithBookInfo bookInfo = mBookInfoList.get(position);
         Intent intent = new Intent(this, BookChapterListActivity.class);
+        intent.putExtra(Constants.BOOK_ID, bookInfo.getBookId());
         startActivity(intent);
     }
 }
